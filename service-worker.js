1f93762a2703b9be8fd03ba96127ffb746a18e9f
Chuwi <chuwi@adsf.es>
2025-08-19 14:31:08 +0200

    27

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-manager-cache-v1.0.26';
+const CACHE_NAME = 'task-manager-cache-v1.0.27';
 const urlsToCache = [
   'https://sasogu.github.io/task-manager-app/',
   'https://sasogu.github.io/task-manager-app/index.html',
