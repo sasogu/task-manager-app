7c783c79ef15d72992e8a41147c740174983825a
Samuel Soriano <sasogu@gmail.com>
2025-08-20 06:55:20 +0200

    1310

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-manager-cache-v1.3.5'; // Versión incrementada
+const CACHE_NAME = 'task-manager-cache-v1.3.10'; // Versión incrementada
 const urlsToCache = [
   'https://sasogu.github.io/task-manager-app/',
   'https://sasogu.github.io/task-manager-app/index.html',
