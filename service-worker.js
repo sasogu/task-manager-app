46b41f0f3a0ffd08001bfbf21bd095eeb93eab3e
Chuwi <chuwi@adsf.es>
2025-08-31 22:53:44 +0200

    botones

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-manager-cache-v1.3.31'; // Versión incrementada
+const CACHE_NAME = 'task-manager-cache-v1.3.33'; // Versión incrementada
 const urlsToCache = [
   'https://sasogu.github.io/task-manager-app/',
   'https://sasogu.github.io/task-manager-app/index.html',
